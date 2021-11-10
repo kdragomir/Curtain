22842456643f35b13badb3f0deecfeabd9329d5a
Lily Watterson <watterse@mail.uc.edu>
2021-11-10 16:28:05 -0500

    Starting to implement parsing
    https://github.com/bbondy/abp-filter-parser

--- content_main.js
+++ content_main.js
@@ -5,7 +5,6 @@
 //////////////////////////////////////////////////////////
 
 var srcNodeList = document.querySelectorAll('[src],[href],[id]');
-var filez = "/block_list_master.txt";
 var worklist;
 
 function readTextFile(file)
@@ -15,7 +14,7 @@ function readTextFile(file)
     rawFile.open("GET", file);
     rawFile.onreadystatechange = function ()
     {
-        console.log("Start func");
+        //console.log("Start func");
         console.log(rawFile.readyState.toString());
 
         if(rawFile.readyState === XMLHttpRequest.DONE)
@@ -25,6 +24,7 @@ function readTextFile(file)
                 //console.log("Fuckin A right dsafafa");
                 worklist = rawFile.responseText;
                 console.log(worklist);
+                splitandtesttext(worklist);
             }
             else
             {
@@ -35,7 +35,29 @@ function readTextFile(file)
     rawFile.send();
 }
 
+
+function splitandtesttext(masstext)
+{
+    var testtext = masstext.split("\n");
+
+    for (var i = 0; i < srcNodeList.length; ++i)
+    {
+        for (var g = 0; g < testtext.length; ++g)
+        {
+            var obj = srcNodeList[i];
+            var testString = testtext[g].split("") - testtext[g].split("")
+
+            if(obj.getAttribute('src') == testString)
+            {
+                obj.remove();
+            }
+        }
+    }
+}
+
 readTextFile("http://curtain.lilywattersongames.com/block_list_master.txt");
+
+
 
 
 
