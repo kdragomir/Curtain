7b36b3eccfbf1c745c46cac2f00089d398baedad
Kai Dragomir <kai.o.dragomir@gmail.com>
2022-01-14 16:10:52 -0500

    fixed incorrect instantiation of the variable

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,22 +1,8 @@
-var switchStatus = false
-var filterSwitchElement= document.getElementById("filter-switch")
+
 function determineIfFilterOn(){
-    var filterSwitchElement = document.getElementById("filter-switch")
-    if(filterSwitchElement.checked){
-        switchStatus = filterSwitchElement.is(':checked');
-        alert(switchStatus)
-    }
+    var filterSwitchElement= document.getElementById("filter-switch").checked;
+    alert(filterSwitchElement);
 }
-// $("#filter-switch").on('change', function(){
-//     if(($this).is(':checked')){
-//         switchStatus = $(this.is(':checked'));
-//         alert(switchStatus); //Verfifies status in the window
-//     }
-//     else{
-//         switchStatus = $(this).is(':checked');
-//         alert(switchStatus);//verifies
-//     }  
-// });
 
 // "adblock-switch"
 // "logging-switch"
