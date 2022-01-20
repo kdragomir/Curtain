3a3646263033419d2f117329bd395ef868935aaf
Kai Dragomir <kai.o.dragomir@gmail.com>
2022-01-20 14:21:34 -0500

    updated js to include correct calls

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,9 +1,20 @@
 
 function determineIfFilterOn(){
-    var filterSwitchElement= document.getElementById("filter-switch").checked;
-    alert(filterSwitchElement);
+    console.log(document.getElementById("filter-switch").checked);
+    return document.getElementById("filter-switch").checked;
 }
-
+function determineIfFilterOn(){
+    return document.getElementById("adblock-switch").checked;
+}
+function determineIfLoggingIsOn(){
+    return document.getElementById("logging-switch").checked;
+}
+function determineIfCookieIsOn(){
+    return document.getElementById("cookie-switch").checked;
+}
+function determineIfFilterOn(){
+    return document.getElementById("statistics-switch").checked;
+}
 // "adblock-switch"
 // "logging-switch"
 // "cookie-switch"
