cef1bd2ab5cb2a845406adab1679c11d6850e8e0
Kai Dragomir <kai.o.dragomir@gmail.com>
2022-01-31 21:41:34 -0500

    added comments for hand over purposes

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -18,13 +18,15 @@ function determineIfStatsOn(){
     console.log(stats);
     return document.getElementById("statistics-switch").checked;
 }
+// This function call serves, to call the display stats function which is supposed to trigger the stats being displayed or Not
+document.getElementById('statistics-switch').addEventListener("click",displayStats());
 
 function displayStats(){
     var stats = document.getElementById('statistics-switch');
-    var displaySetting = document.getElementById('quick-stats');
-    if (determineIfStatsOn()){ 
-        displaySetting.style.display = "none";
-        }
+    var displaySetting = document.getElementById('quick-stats');  //To reference the quick stats element which is to be displayed or not.
+    if (determineIfStatsOn()){  //This looks strange, but because the switch itself is a "checkbox" if it is "active", then it will return False
+        displaySetting.style.display = "none"; //So since it is "checked" it becomes unactive, also we have come to an consensus that the options in the popup menu will be "active" by default
+        } 
     else{
         displaySetting.style.display = "inline";
     }
